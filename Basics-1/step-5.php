5e618179cc09ab1f36930fde1a038b9a3812ceca
AhmadBreiss <ahmatobreiss@gmail.com>
2023-02-20 00:55:40 +0200

    step-5.php

---
+++ Basics-1/step-5.php
@@ -0,0 +1,11 @@
+<?php
+$course_name="American Center";
+$enrolled_students= 40;
+$price= 115.5 ;
+$on_discount= false;
+echo "Course title: $course_name  
+ Enrolled Studnets: $enrolled_students 
+ Course price: $price $ USD
+ Course on discount: ";
+ echo $on_discount ? 'yes' :'No' ."\n";
+?>
