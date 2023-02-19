dec834ae5a71d351487b77d050fef69a64795cd7
AhmadBreiss <ahmatobreiss@gmail.com>
2023-02-20 00:52:38 +0200

    step-3.php

--- Basics-1/step-2.php
+++ Basics-1/step-2.php
@@ -1,5 +1,5 @@
 "Hello world, Help me I am using PHP"
 <?php
 print "I am getting errors but I can't see the errors";
-echo  "Search on how to enable PHP Errors"
+echo  " earch on how to enable PHP Errors"
 ?>
