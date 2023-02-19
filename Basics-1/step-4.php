453e36147d08bcd56f37e38662ed02cd9363bfc3
AhmadBreiss <ahmatobreiss@gmail.com>
2023-02-20 00:55:01 +0200

    step-4.php

---
+++ Basics-1/step-4.php
@@ -0,0 +1,16 @@
+<?php
+//The first method
+
+$name='Ahmad breiss';
+$position='tech consultant';
+$github_url='https://github.com/AhmadBreiss';
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link : ". $github_url."\n";
+
+//The second method
+
+echo "Hello, I'm {$name}, I'm a {$position} please check my github link : {$github_url} \n";
+
+// And the third method 
+
+echo 'Hello, I m '.$name.", I'm a ".$position.' please check my github link : '.$github_url ;
+?>
