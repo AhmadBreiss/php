70029d2407d4d4cc36a225291c0ea713557f08c2
AhmadBreiss <ahmatobreiss@gmail.com>
2023-02-20 00:53:13 +0200

    step-3.php

---
+++ Basics-1/step-3.php
@@ -0,0 +1,9 @@
+<?php
+$name = "Ahmad Breiss";
+$email = "ahmatobreiss@gmail.com";
+print" My name is : ". $name ." and my email is : ". $email . "\n";
+$height = 187;
+$width = 80;
+$area = $height*$width ;
+echo "The area is: ".$area ;
+?>
